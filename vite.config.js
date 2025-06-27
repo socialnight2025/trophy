cd812b1c3067cb1736986ea268f1c4ea51b17e7a
webasiWebApp <webasiinfo@gmail.com>
2025-06-28 02:45:38 +0530

    first commit2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/trophy',
+  base: '/trophy/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
